3220cc4577236af0ff8a3cbdc724f618c7b27381
Moc <m.tagarro@alumnos.upm.es>
2026-09-20 01:05:42 +0200

    Moved resource files location

--- src/nfl/utils/_resources.py
+++ src/nfl/utils/_resources.py
@@ -3,12 +3,12 @@ from importlib.resources import files
 
 
 def read_resource_file(file: str) -> str:
-    resource = files("nfl.io") / "files" / file
+    resource = files("nfl") / "resources" / file
     return resource.read_text(encoding="utf-8")
 
 
 def read_resource_file_as_stream(file: str) -> Iterator[str]:
-    resource = files("nfl.io") / "files" / file
+    resource = files("nfl") / "resources" / file
 
     with resource.open("r", encoding="utf-8") as f:
         yield from f
